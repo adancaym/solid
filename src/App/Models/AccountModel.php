422f53a8abf431e95ce150e0b4f0f59a0a24ee3f
Adan <adanyanezgonzalez@gmail.com>
2019-08-14 23:56:04 -0500

    Solo falta comprobar los SP de retiro, retiro credito y deposito en las API REST

--- src/App/Models/AccountModel.php
+++ src/App/Models/AccountModel.php
@@ -15,11 +15,16 @@ class AccountModel extends Model
     }
     public function getAcountByAccountAndIdUser($account,$iduser){
         return $this->getDao()->
-        select('account.*,((sum_arit_by_idaccount(account.idaccount)+account.limit_money)/1.1) as monto_credito,abs(sum_arit_by_idaccount(account.idaccount)) monto,account_type.account_type')->
+        select('
+        account.*,
+        ((sum_arit_by_idaccount(account.idaccount)+account.limit_money)/1.1) as monto_credito,
+        abs(sum_arit_by_idaccount(account.idaccount)) monto,
+        account_type.account_type,
+        if(account_type.idaccount_type = 1, account.limit_money, ((sum_arit_by_idaccount(account.idaccount)+account.limit_money)/1.1) ) as maximo
+        ')->
         from()->
         join('account_type','idaccount_type')->
         where('account',$account)->and('iduser',$iduser)->get(true);
-
     }
     
 
